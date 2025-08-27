1714ce270fc0d4f03cb803630b368e564e16196c
William.io <capuletos@live.com>
2025-08-27 16:55:43 -0300

    feat(application): #002 criada camada aplication e adicao das interfaces para implementacao do CQRS

---
+++ Source/Adset.Lead.Application/Automobiles/CreateAutomobile/CreateAutomobileCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Input;
+using Adset.Lead.Application.Abstractions.CQRSCommunication;
+
+namespace Adset.Lead.Application.Automobiles.CreateAutomobile;
+
+public sealed record CreateAutomobileCommand(
+    string Brand,
+    string Model,
+    int Year,
+    string Plate,
+    string Color,
+    decimal Price,
+    int? Km,
+    IReadOnlyCollection<string>? OptionalFeatures,
+    IReadOnlyCollection<string>? PhotoUrls) : ICommand<Guid>;
---
+++ Source/Adset.Lead.Application/Automobiles/CreateAutomobile/CreateAutomobileCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using Adset.Lead.Application.Abstractions.CQRSCommunication;
+using Adset.Lead.Domain.Abstractions;
+
+namespace Adset.Lead.Application.Automobiles.CreateAutomobile;
+
+internal sealed class CreateAutomobileCommandHandler : ICommandHandler<CreateAutomobileCommand, Guid> 
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateAutomobileCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public Task<Result<Guid>> Handle(CreateAutomobileCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}

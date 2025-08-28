ff2085d911099cf9deab4ebf8c0573b1df12d9c2
William.io <capuletos@live.com>
2025-08-28 10:20:44 -0300

    feat(domain): #007 adicionado eventos de dominio para registro exclusao e atualizacoes de automoveis

--- Source/Adset.Lead.Domain/Abstractions/Entity.cs
+++ Source/Adset.Lead.Domain/Abstractions/Entity.cs
@@ -4,10 +4,7 @@ public abstract class Entity
 {
     private readonly List<IDomainEvent> _domainEvents = new();
     
-    protected Entity(Guid id)
-    {
-        Id = id;
-    }
+    protected Entity(Guid id) => Id = id;
     
     protected Entity() {}
     public Guid Id { get; set; }
--- Source/Adset.Lead.Domain/Automobiles/Automobile.cs
+++ Source/Adset.Lead.Domain/Automobiles/Automobile.cs
@@ -1,4 +1,5 @@
 ﻿using Adset.Lead.Domain.Abstractions;
+using Adset.Lead.Domain.Automobiles.Events;
 
 namespace Adset.Lead.Domain.Automobiles;
 
@@ -12,7 +13,7 @@ public sealed class Automobile : Entity
         int? km,
         string color,
         decimal price,
-        List<OptionalFeatures> features,
+        OptionalFeatures features,
         List<Photo> photos) : base(id)
     {
         Brand = brand;
@@ -28,9 +29,8 @@ public sealed class Automobile : Entity
 
     private Automobile()
     {
-        
     }
-
+    
     public string Brand { get; private set; } = null!;
     public string Model { get; private set; } = null!;
     public int Year { get; private set; }
@@ -39,6 +39,113 @@ public sealed class Automobile : Entity
     public string Color { get; private set; } = null!;
     public decimal Price { get; private set; }
 
-    public List<OptionalFeatures> Features { get; private set; } = new();
+    public OptionalFeatures Features { get; private set; }
     public List<Photo> Photos { get; set; } = new();
+    public PortalPackage? PortalPackage { get; private set; }
+    
+    public static Automobile RegisterAnNewAutomobile(
+        string brand,
+        string model,
+        int year,
+        string plate,
+        int? km,
+        string color,
+        decimal price,
+        OptionalFeatures features,
+        List<Photo> photos,
+        Portal portal,
+        Package package)
+    {
+        var automobile = new Automobile(
+            Guid.NewGuid(),
+            brand,
+            model,
+            year,
+            plate,
+            km,
+            color,
+            price,
+            features,
+            photos);
+
+        // Define o portal e package no momento da criação
+        var portalPackage = new PortalPackage(
+            Guid.NewGuid(),
+            automobile.Id,
+            portal,
+            package);
+        
+        automobile.PortalPackage = portalPackage;
+
+        automobile.RaiseDomainEvent(new AutomobileRegisteredDomainEvent(automobile.Id));
+
+        return automobile;
+    }
+
+    public Result Update(string brand, string model, string plate, string color, decimal price,
+        OptionalFeatures features, List<Photo> photos)
+    {
+        if (Id == Guid.Empty)
+            return Result.Failure(AutomobileErrors.NotActive);
+        
+        Brand = brand;
+        Model = model;
+        Plate = plate;
+        Color = color;
+        Price = price;
+        Features = features;
+        Photos = photos;
+        
+        return Result.Success();
+    }
+    
+    public Result SetPortalPackageLast(Portal portal, Package package)
+    {
+        if (Id == Guid.Empty)
+            return Result.Failure(AutomobileErrors.NotActive);
+
+        var portalPackage = new PortalPackage(
+            Guid.NewGuid(),
+            Id,
+            portal,
+            package);
+
+        PortalPackage = portalPackage;
+        
+        return Result.Success();
+    }
+
+    public Result UpdatePortalPackage(Portal portal, Package package)
+    {
+        if (Id == Guid.Empty)
+            return Result.Failure(AutomobileErrors.NotActive);
+
+        if (PortalPackage == null)
+            return Result.Failure(AutomobileErrors.PortalPackageNotSet);
+
+        // Cria um novo PortalPackage com os novos valores
+        var updatedPortalPackage = new PortalPackage(
+            PortalPackage.Id, // Mantém o mesmo Id
+            Id,
+            portal,
+            package);
+
+        PortalPackage = updatedPortalPackage;
+        
+        return Result.Success();
+    }
+
+    public Result RemovePortalPackage()
+    {
+        if (Id == Guid.Empty)
+            return Result.Failure(AutomobileErrors.NotActive);
+
+        if (PortalPackage == null)
+            return Result.Failure(AutomobileErrors.PortalPackageNotSet);
+
+        PortalPackage = null;
+        
+        return Result.Success();
+    }
+
 }
